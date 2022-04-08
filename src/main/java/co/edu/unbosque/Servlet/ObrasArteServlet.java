d04327bd6ad3ccfaeba4df97c8ba837711f34bfb
Natalia Vernano <you@example.com>
2022-04-08 11:56:53 -0500

    ver piezas

--- src/main/java/co/edu/unbosque/Servlet/ObrasArteServlet.java
+++ src/main/java/co/edu/unbosque/Servlet/ObrasArteServlet.java
@@ -1,6 +1,7 @@
 package co.edu.unbosque.Servlet;
 
 import co.edu.unbosque.Datos.DatosObrasArte;
+import com.opencsv.CSVWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -24,6 +26,7 @@ public class ObrasArteServlet extends HttpServlet {
     private String RUTA_SUBIDA= "subidos";
     private ArrayList<DatosObrasArte> datosObrasArteArrayList = new ArrayList<DatosObrasArte>();
 
+
     public void init() {
 
     }
@@ -32,12 +35,13 @@ public class ObrasArteServlet extends HttpServlet {
         System.out.println("Title: "+request.getParameter("title")+"\n" +
                 "Price: "+request.getParameter("precio"));
         String t = request.getParameter("title");
+
         int p = Integer.parseInt(request.getParameter("precio")) ;
 
         String rutaArchivo = getServletContext().getRealPath("")+ File.separator+RUTA_SUBIDA;
         File DirArchivo = new File(rutaArchivo);
 
-
+       CSVWriter writer = new CSVWriter(new FileWriter("obras.csv"));
 
         if(!DirArchivo.exists()) DirArchivo.mkdir();
         File imagen = null;
@@ -53,6 +57,17 @@ public class ObrasArteServlet extends HttpServlet {
         }
         DatosObrasArte obra = new DatosObrasArte(t,p,imagen);
         datosObrasArteArrayList.add(obra);
+        for (int i = 0; i<datosObrasArteArrayList.size(); i++){
+            String[] linea = new String[5];
+            linea[0] = datosObrasArteArrayList.get(i).getTitulo();
+            linea[1]= ",";
+            linea[2]= String.valueOf(datosObrasArteArrayList.get(i).getPrecio());
+            linea[3]=",";
+            linea[4]= String.valueOf(datosObrasArteArrayList.get(i).getImagen());
+            writer.writeNext(linea);
+        }
+        writer.close();
+
        System.out.println(datosObrasArteArrayList.toString());
        response.sendRedirect("./prueba.html");
    }
